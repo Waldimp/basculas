fc66fe9f087606298ec682368640804a14391d2e
Waldimp <waltermejia61@hotmail.com>
2024-06-18 17:14:12 -0600

    Proyecto creado

---
+++ src/main/java/com/conexiontcp/basculas/BasculasApplication.java
@@ -0,0 +1,13 @@
+package com.conexiontcp.basculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conexiontcp/basculas/BasculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexiontcp.basculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
